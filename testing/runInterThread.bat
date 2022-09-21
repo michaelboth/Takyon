2a81ad40ba1debc1eb5784f253b50f7d8c080b31
Michael Both <michael23.both@gmail.com>
2022-09-21 15:38:29 -0400

    New File

---
+++ testing/runInterThread.bat
@@ -0,0 +1,30 @@
+@echo off
+
+rem Keep variables local to this script
+setlocal EnableDelayedExpansion
+
+rem hello-two_sided
+cd ..\examples\hello-two_sided
+if ERRORLEVEL 1 ( echo "Failed to cd ..\examples\hello-two_sided" & GOTO:done )
+hello_mt "InterThread -pathID=1" 0
+if ERRORLEVEL 1 ( echo "Failed to run hello-two_sided" & GOTO:done )
+hello_mt "InterThread -pathID=1" 1
+if ERRORLEVEL 1 ( echo "Failed to run hello-two_sided" & GOTO:done )
+hello_mt "InterThread -pathID=1" 10
+if ERRORLEVEL 1 ( echo "Failed to run hello-two_sided" & GOTO:done )
+
+rem hello-one_sided
+cd ..\hello-one_sided
+if ERRORLEVEL 1 ( echo "Failed to cd ..\examples\hello-one_sided" & GOTO:done )
+hello_mt "InterThread -pathID=1" 0
+if ERRORLEVEL 1 ( echo "Failed to run hello-one_sided" & GOTO:done )
+hello_mt "InterThread -pathID=1" 1
+if ERRORLEVEL 1 ( echo "Failed to run hello-one_sided" & GOTO:done )
+hello_mt "InterThread -pathID=1" 10
+if ERRORLEVEL 1 ( echo "Failed to run hello-one_sided" & GOTO:done )
+
+GOTO:done
+
+:done
+endlocal
+GOTO:eof
